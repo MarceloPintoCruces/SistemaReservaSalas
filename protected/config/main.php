da4211eebf0f691969632f53aa5658ce52cd6b0a
MarceloPintoCruces <marcelo.pinto.cruces@outlook.com>
2014-09-05 10:34:39 -0400

    Modificacion del main

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Sistema de Reserva de Salas',
+	'name'=>'Sistema de Reserva de Salas UBB',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
